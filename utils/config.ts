6e144f42656472d785a38f67468d356fe8477db2
Cadu de Castro Alves <cadudecastroalves@gmail.com>
2022-08-12 16:04:04 +0200

    Fix typo

--- utils/config.ts
+++ utils/config.ts
@@ -2,5 +2,5 @@ export const config = {
     API_URL:
         process.env.NODE_ENV !== "development"
             ? process.env.VERCEL_URL
-            : "http://localhost:3001";
+            : "http://localhost:3001",
 };
